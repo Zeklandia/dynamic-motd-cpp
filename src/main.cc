d399beff0f9ffb5e702de872894fe08d1418bc1b
Asher Wood <awood5659@gmail.com>
2013-08-15 18:20:14 -0400

    Add new color formatting options:  - Light  - Italicized  - Underlined
    Added ncurses to building, and started testing ncurses
     - need to convert all output to ncurses friendly stuff
     - design ncurses window

--- src/main.cc
+++ src/main.cc
@@ -4,21 +4,29 @@
 #include <string.h>
 #include <stdio.h>
 #include <cstdlib>
+
 #include <time.h>
+
 #include <netdb.h>
-#include <sys/param.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <net/if.h>
+
 #include <fstream>
+
 #include <unistd.h>
 #include <sys/utsname.h>
+#include <sys/param.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
-#include <netinet/in.h>
-#include <net/if.h>
+
 #include <sys/sysinfo.h>
 #include <linux/sysinfo.h>
 #include <linux/kernel.h>
+
+#include <utmp.h>
+
 #include <ncurses.h>
 
 using namespace std;
@@ -30,23 +38,48 @@ int main()
 	system("reset");
 
 	//Set colors
-	string color_default	("\e[0;0m");
-	string red				("\e[0;31m");
-	string red_bold			("\e[1;31m");
-	string green			("\e[0;32m");
-	string green_bold		("\e[1;32m");
-	string yellow			("\e[0;33m");
-	string yellow_bold		("\e[1;33m");
-	string blue				("\e[0;34m");
-	string blue_bold		("\e[1;34m");
-	string magenta			("\e[0;35m");
-	string magenta_bold		("\e[1;35m");
-	string cyan				("\e[0;36m");
-	string cyan_bold		("\e[1;36m");
-	string gray				("\e[0;37m");
-	string gray_bold		("\e[1;37m");
-	string white			("\e[0;97m");
-	string white_bold		("\e[1;97m");
+	//     Color				ASCII Code
+	string color_default		("\e[0;0m");
+	string red					("\e[0;31m");
+	string red_bold				("\e[1;31m");
+	string red_light			("\e[2;31m");
+	string red_italic			("\e[3;31m");
+	string red_underlined		("\e[4;31m");
+	string green				("\e[0;32m");
+	string green_bold			("\e[1;32m");
+	string green_light			("\e[2;32m");
+	string green_italic			("\e[3;32m");
+	string green_underlined		("\e[4;32m");
+	string yellow				("\e[0;33m");
+	string yellow_bold			("\e[1;33m");
+	string yellow_light			("\e[2;33m");
+	string yellow_italic		("\e[3;33m");
+	string yellow_underlined	("\e[4;33m");
+	string blue					("\e[0;34m");
+	string blue_bold			("\e[1;34m");
+	string blue_light			("\e[2;34m");
+	string blue_italic			("\e[3;34m");
+	string blue_underlined		("\e[4;34m");
+	string magenta				("\e[0;35m");
+	string magenta_bold			("\e[1;35m");
+	string magenta_light		("\e[2;35m");
+	string magenta_italic		("\e[3;35m");
+	string magenta_underlined	("\e[4;35m");
+	string cyan					("\e[0;36m");
+	string cyan_bold			("\e[1;36m");
+	string cyan_light			("\e[2;36m");
+	string cyan_italic			("\e[3;36m");
+	string cyan_underlined		("\e[4;36m");
+	string gray					("\e[0;37m");
+	string gray_bold			("\e[1;37m");
+	string gray_light			("\e[2;37m");
+	string gray_italic			("\e[3;37m");
+	string gray_underlined		("\e[4;37m");
+	string white				("\e[0;97m");
+	string white_bold			("\e[1;97m");
+	string white_light			("\e[2;97m");
+	string white_italic			("\e[3;97m");
+	string white_underlined		("\e[4;97m");
 
 	//System stats
 	struct utsname uname_info;
@@ -156,34 +189,35 @@ int main()
 	system("reset");
 
 	/*
-	//Print the loaded information
-	cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=[ " << red_bold << "System Stats" << cyan << " ]=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl
-		<< cyan << ":   " << white_bold << "Date/Time" << cyan << " = " << green_bold << datetime << endl
-		<< cyan << ":    " << white_bold << "Hostname" << cyan << " = " << green_bold << hostname << endl
-		<< cyan << ":     " << white_bold << "WLAN IP" << cyan << " = " << green_bold << wlan_ip << endl
-		<< cyan << ":      " << white_bold << "LAN IP" << cyan << " = " << green_bold << lan_ip << endl
-		<< cyan << ":      " << white_bold << "Kernel" << cyan << " = " << green_bold << kernel_version << endl
-		<< cyan << ":      " << white_bold << "Memory" << cyan << " = " << green_bold << ram_size << " mB" << endl
-		<< cyan << ":       " << white_bold << "Stats" << cyan << " = " << green_bold << user_stats << endl;
+	 * Old output
+	 *  cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=[ " << red_bold << "System Stats" << cyan << " ]=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl
+	 *  		<< cyan << ":   " << white_bold << "Date/Time" << cyan << " = " << green_bold << datetime << endl
+	 *  		<< cyan << ":    " << white_bold << "Hostname" << cyan << " = " << green_bold << hostname << endl
+	 *  		<< cyan << ":     " << white_bold << "WLAN IP" << cyan << " = " << green_bold << wlan_ip << endl
+	 *  		<< cyan << ":      " << white_bold << "LAN IP" << cyan << " = " << green_bold << lan_ip << endl
+	 *  		<< cyan << ":      " << white_bold << "Kernel" << cyan << " = " << green_bold << kernel_version << endl
+	 *  		<< cyan << ":      " << white_bold << "Memory" << cyan << " = " << green_bold << ram_size << " mB" << endl
+	 *  		<< cyan << ":       " << white_bold << "Stats" << cyan << " = " << green_bold << user_stats << endl;
+	 *
+	 *  cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:[ " << red_bold << "User Stats" << cyan << " ]:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl
+	 *  		<< cyan << ":    " << white_bold << "Username" << cyan << " = " << green_bold << username << endl
+	 *  		<< cyan << ":  " << white_bold << "Last Login" << cyan << " = " << green_bold << last_login << endl
+	 *  		<< cyan << ":	" << white_bold << "Home" << cyan << " = " << green_bold << home_path << endl
+	 *  		<< cyan << ":   " << white_bold << "Processes" << cyan << " = " << green_bold << process_count << " of " << process_limit << endl;
+	 * 
+	 *  cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:[ " << red_bold << "Internet Weather Stats" << cyan << " ]:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl
+	 *  		<< cyan << ":    " << white_bold << "Location" << cyan << " = " << green_bold << weather_location << ", " << weather_zip << endl
+	 *  		<< cyan << ":	   " << white_bold << "Temp" << cyan << " = " << green_bold << weather_temperature << endl
+	 *  		<< cyan << ":	" << white_bold << "Weather" << cyan << " = " << green_bold << weather_conditions << endl
+	 *  		<< cyan << ":	 " << white_bold << "Sky" << cyan << " = " << green_bold << weather_sky_conditions << endl;
+	 *
+	 *  cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=[ " << red_bold << custom_header << cyan << " ]=:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl
+	 *  		<< cyan << ":    " << white_bold << custom_command << cyan << " = " << green_bold << custom_command_output << endl;
+	 *
+	 *  cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:[]:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:"
+	 *  		<< color_default << endl << endl; */
 
-	cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:[ " << red_bold << "User Stats" << cyan << " ]:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl
-		<< cyan << ":    " << white_bold << "Username" << cyan << " = " << green_bold << username << endl
-		<< cyan << ":  " << white_bold << "Last Login" << cyan << " = " << green_bold << last_login << endl
-		<< cyan << ":	" << white_bold << "Home" << cyan << " = " << green_bold << home_path << endl
-		<< cyan << ":   " << white_bold << "Processes" << cyan << " = " << green_bold << process_count << " of " << process_limit << endl;
-
-	cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:[ " << red_bold << "Internet Weather Stats" << cyan << " ]:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl
-		<< cyan << ":    " << white_bold << "Location" << cyan << " = " << green_bold << weather_location << ", " << weather_zip << endl
-		<< cyan << ":	   " << white_bold << "Temp" << cyan << " = " << green_bold << weather_temperature << endl
-		<< cyan << ":	" << white_bold << "Weather" << cyan << " = " << green_bold << weather_conditions << endl
-		<< cyan << ":	 " << white_bold << "Sky" << cyan << " = " << green_bold << weather_sky_conditions << endl;
-
-	cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=[ " << red_bold << custom_header << cyan << " ]=:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl
-		<< cyan << ":    " << white_bold << custom_command << cyan << " = " << green_bold << custom_command_output << endl;
-
-	std::cout << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:[]:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:"
-		<< color_default << endl << endl;
-		*/
+	//Stable output
 	cout << cyan << "Dynamic-MOTD-CPP" << color_default << endl;
 	cout	<< "Date/Time:	" << datetime 				<< endl
 			<< "Hostname:	" << hostname 				<< endl
@@ -202,5 +236,13 @@ int main()
 			<< "Temp:		" << weather_temperature	<< endl
 			<< "Weather:	" << weather_conditions		<< endl
 			<< "Sky:		" << weather_sky_conditions	<< endl;
+
+	//ncurses testing output
+	initscr();
+	printw("Hostname:	");
+	printw(hostname);
+	refresh();
+	getch();
+	endwin();
 	return 0;
 }
